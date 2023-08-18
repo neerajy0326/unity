d83b3ea56ac8d201a0e237dbda0f4db01c730290
neerajy0326 <neerajy0326@gmail.com>
2023-08-18 23:58:56 +0530

    updated

--- unity/settings.py
+++ unity/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-import os
-from celery import Celery
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
