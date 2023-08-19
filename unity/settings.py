2b4ac1db9cb27a3636bda966912876dd0012df53
neerajy0326 <neerajy0326@gmail.com>
2023-08-19 16:00:47 +0530

    updated

--- unity/settings.py
+++ unity/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t1ptfnrd#7axti@f6y^5wjt$y0fpmr#0##46hpulzz+k%a$5(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 TIME_ZONE = 'Asia/Kolkata'
 USE_TZ = True
 ALLOWED_HOSTS = ['*']
@@ -123,14 +123,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,"static")
  ]
-
+STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
 COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
